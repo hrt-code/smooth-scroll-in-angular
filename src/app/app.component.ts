ed4e0d4d540b62ace85388b55e0120986a9a4992
HamidReza Torbatian <78443561+hrt-code@users.noreply.github.com>
2022-10-02 22:29:32 +0330

    add smooth scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smooth-scroll';
+
+
+  isShowScrollTop: boolean = false;
+  isShowScrollBottom: boolean = false;
+  topPosToStartShowing = 100;
+  bottomPosToEndShowing = 1000;
+
+  constructor() { }
+
+
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if ((scrollPosition + this.bottomPosToEndShowing) < document.body.scrollHeight)
+      this.isShowScrollBottom = true
+    else
+      this.isShowScrollBottom = false;
+
+
+    if (scrollPosition >= this.topPosToStartShowing)
+      this.isShowScrollTop = true;
+    else
+      this.isShowScrollTop = false;
+
+  }
+
+
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  gotoDown() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+  }
+
+
 }
